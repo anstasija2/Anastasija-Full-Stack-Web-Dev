3e79865884822c5d51b391efb1a409d1635e2d6f
Anastasija Galicka <anastasija.galicka@Anastasijas-MacBook-Pro-2.local>
2023-02-25 11:22:17 +0000

    done

---
+++ hash_test/hash.rb
@@ -0,0 +1,14 @@
+
+AGE_OF_STUDENTS = {
+  "Laura" => 23,
+  "Peter" => 20,
+  "Mary" => 27
+}
+
+AGE_OF_STUDENTS["Laura"]
+
+AGE_OF_STUDENTS["jane"]= 28
+
+AGE_OF_STUDENTS["Mary"]=28
+
+AGE_OF_STUDENTS.delete_at("Mary")
